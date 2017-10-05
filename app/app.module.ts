afdb116ae2c28d0f0d80b19fab76c6c24d5f8a44
Caue Queiroz <cauenqueiroz@gmail.com>
2017-10-05 14:22:43 -0300

    Reset initial scafolding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
-
-import { ItemService } from "./item/item.service";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
-
-// Uncomment and add to NgModule imports if you need to use two-way binding
-// import { NativeScriptFormsModule } from "nativescript-angular/forms";
-
-// Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
-// import { NativeScriptHttpModule } from "nativescript-angular/http";
 
 @NgModule({
     bootstrap: [
@@ -22,12 +13,7 @@ import { ItemDetailComponent } from "./item/item-detail.component";
         AppRoutingModule
     ],
     declarations: [
-        AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
-    ],
-    providers: [
-        ItemService
+        AppComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
