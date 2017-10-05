987ee8cbcd1f9e5fed962b2a12d698d9a7abe1d1
Caue Queiroz <cauenqueiroz@gmail.com>
2017-10-05 14:51:07 -0300

    Upgrading routing system

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,29 +1,20 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { routes, navigableComponents } from "./app.routing";
 
-import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
-import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        NativeScriptRouterModule,
+        NativeScriptRouterModule.forRoot(routes)
     ],
     declarations: [
         AppComponent,
-        LoginComponent,
-        HomeComponent
+        ...navigableComponents
     ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+    bootstrap: [ AppComponent ]
 })
-/*
-Pass your application module to the bootstrapModule function located in main.ts to start your app
-*/
 export class AppModule { }
